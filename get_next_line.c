9d46a981e0a2b97daa85052e38e4a8403ba271a4
Lorys LOPEZ <llopez@e1r1p11.42.fr>
2017-12-03 16:12:06 +0100

    saves

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 14:51:28 by llopez            #+#    #+#             */
-/*   Updated: 2017/12/02 18:06:37 by llopez           ###   ########.fr       */
+/*   Updated: 2017/12/03 16:08:56 by llopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ static int		find_break(char *str)
 	while (str[i])
 	{
 		if (str[i++] == '\n')
+		{
 			return (i - 1);
+		}
 	}
 	return (-1);
 }
@@ -30,9 +32,10 @@ int		get_next_line(const int fd, char **line)
 	int			i_buff;
 	char		line_read[BUFF_SIZE + 1];
 	static char *buff;
-	char		*tmp;
 	int			break_found;
 
+	if (fd < 0 || read(fd, NULL, 0) == -1 || line == NULL)
+		return (-1);
 	if (buff != NULL)
 	{
 		if (find_break(buff) > -1)
@@ -40,50 +43,38 @@ int		get_next_line(const int fd, char **line)
 			break_found = find_break(buff);
 			*line = ft_strnew((size_t)break_found);
 			ft_strncpy(*line, buff, (size_t)break_found);
-			tmp = ft_strnew(ft_strlen(buff) - break_found);
-			tmp = ft_strsub(buff, (break_found + 1), \
-					(ft_strlen(buff) - (break_found + 1)));
-			free(buff);
-			buff = ft_strnew(ft_strlen(tmp));
-			ft_strcpy(buff, tmp);
-			free(tmp);
+			ft_strcpy(buff, (buff + break_found + 1));
 			return (1);
 		}
-		else{
-			*line = ft_strnew(ft_strlen(buff));
-			*line = ft_strsub(buff, 1, ft_strlen(buff) - 1);
+		else {
+			*line = ft_strdup(buff);
+			free(buff);
+			buff = ft_strnew(0);
 		}
 	}
 	else {
+		printf("buff == NULL\n");
 		buff = ft_strnew(0);
+		*line = ft_strnew(0);
 	}
-	*line = ft_strnew(0);
 	line_read[BUFF_SIZE] = '\0';
 	while ((i_buff = read(fd, line_read, BUFF_SIZE)) > 0)
 	{
-		printf("line_read : %s\n", line_read);
+		printf("lu : %d chars\n", i_buff);
 		if (find_break(line_read) > -1)
 		{
-			tmp = ft_strnew(ft_strlen(*line));
-			ft_strcpy(tmp, *line);
-			free(*line);
-			*line = ft_strnew(ft_strlen(*line) + ft_strlen(buff));
-			ft_strcpy(*line, tmp);
-			*line = ft_strncat(*line, line_read, (size_t)find_break(line_read));
-			buff = ft_strsub(line_read, find_break(line_read)+1, (BUFF_SIZE \
-					- find_break(line_read)));
-			break;
+			break_found = find_break(line_read);
+			line_read[break_found] = '\0';
+			*line = ft_strjoin(*line, line_read);
+			buff = ft_strnew(ft_strlen(line_read + break_found + 1));
+			ft_strcpy(buff, (line_read + break_found + 1));
+			return (1);
 		}
 		else {
-			tmp = ft_strnew(ft_strlen(*line));
-			ft_strcpy(tmp, *line);
-			free(*line);
-			*line = ft_strnew(ft_strlen(*line) + ft_strlen(buff));
-			ft_strcpy(*line, tmp);
-			*line = ft_strcat(*line, line_read);
+			*line = ft_strjoin(*line, line_read);
 		}
 	}
-	if (i_buff == 0)
-		return (0);
-	return (1);
+	if (ft_strlen(*line) > 0 || i_buff > 0)
+		return (1);
+	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 16:54:17 by llopez            #+#    #+#             */
-/*   Updated: 2017/11/29 13:33:47 by llopez           ###   ########.fr       */
+/*   Updated: 2017/12/03 14:33:08 by llopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,12 @@ int		main(int argc, char **argv)
 
 	i = 0;
 	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line))
-		printf("%s\n", line);
+	while (get_next_line(fd, &line) > 0)
+	{
+		i++;
+		printf("%d : %s\n", i, line);
+	}
+	printf("\n----------------------\n%d lines shown.\n", i);
 	close(fd);
 	return (0);
 }
